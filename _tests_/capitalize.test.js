76cd4f80853b06d8edb80a8bab9ee99379fb8cb0
LevKrestuaninov <krestuaninovlev@gmail.com>
2023-03-29 11:10:02 +0300

    Revert "add jest test"
    This reverts commit cc69df14aa8ac031557cca37568625873f5bc7e5.

--- _tests_/capitalize.test.js
+++
@@ -1,6 +0,0 @@
-import capitalize from "../src/capitalize";
-
-test ('capitalize', () => {
-    expect(capitalize('')).toEqual('');
-    expect(capitalize('hello')).toEqual('Hello');
-});
---
+++ tests/capitalize.test.js
@@ -0,0 +1,5 @@
+import { strict as assert } from 'node:assert';
+import capitalize from "../src/capitalize.js";
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
+console.log('Все тесты пройдены!');
